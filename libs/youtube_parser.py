0f48ffd707ed339f4543e9ad830d8d4ecbf357a3
SubeljEtra <jan.subelj@kolektor.com>
2020-07-21 14:07:44 +0200

    youtube rendering

--- libs/youtube_parser.py
+++ libs/youtube_parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import json
 
 def get_video_title(vid_id):
     content = requests.get(f"https://www.youtube.com/watch?v={vid_id}")
@@ -10,8 +11,26 @@ def get_video_title(vid_id):
 
 
 if __name__=="__main__":
-    content = requests.get("https://www.youtube.com/user/PewDiePie/videos")
-    soup = bs(content.content, "html.parser")
-    # print(soup)
-    title = soup.find("meta", attrs={"name": "title"}).text
-    print(soup.find("meta", attrs={"name": "title"})["content"])
+    # content = requests.get("https://www.youtube.com/user/PewDiePie/videos")
+    # soup = bs(content.content, "html.parser")
+    # # print(soup)
+    # title = soup.find("meta", attrs={"name": "title"}).text
+    # print(soup.find("meta", attrs={"name": "title"})["content"])
+    import re
+    pattern = re.compile(r'window\["ytInitialData"\] = (.+?);')
+
+    soup = bs(open("test.html","r").read(),"html.parser")
+    data = soup.find("script", text=pattern).contents[0]
+    tabs = json.loads(pattern.search(data).group(1))["contents"]["twoColumnBrowseResultsRenderer"]["tabs"]
+    videos = None
+    for t in tabs:
+        if t["tabRenderer"]["title"] == "Videos":
+            videos = t["tabRenderer"]["content"]
+            break
+    contents = videos["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["gridRenderer"]["items"]
+    print(contents)
+
+
+
+
+    #        contents twoColumnBrowseResultsRenderer tabs    tabRenderer content "sectionListRenderer" "contents": [contents [gridRenderer items gridVideoRenderer]]
